9adc728af67b510a1d9aa022ac1c77f49e7db43f
shravanigandham <shravani.gandham470@gmail.com>
2016-04-30 21:59:15 +0530

    Update Cust_registration.java

--- Cust_registration.java
+++ Cust_registration.java
@@ -1,27 +1,67 @@
 package shoppingmall;
-import java.io.*;
-
-public class Cust_registration {
+public class Cust_registration{
 String cust_name;
 long cust_ph_number;
 String cust_email_id;
-String type_of_customer;
+static String type_of_customer;
  static int num_of_cust;
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-public void setCustomerDetails() throws IOException{
-	System.out.println("enter number of customers entering into the store : ");
-	num_of_cust=Integer.parseInt(br.readLine()); 
-	for(int i=1;i<=num_of_cust;i++){
-		System.out.println("enter customer "+i+" name,phone number,type");
-		cust_name=br.readLine();
-		cust_ph_number=Long.parseLong(br.readLine());
-		type_of_customer=br.readLine();
-		System.out.println("customer"+i);
-		System.out.println("name	:"+cust_name);
-		System.out.println("phone no	:"+cust_ph_number);
-		System.out.println("heavy of light cusstomer :"+type_of_customer);
-		System.out.println("\n");
-		}
+
+public Cust_registration(String cust_name, long cust_ph_number,
+		String cust_email_id, String type_of_customer) {
+	
+	this.cust_name = cust_name;
+	this.cust_ph_number = cust_ph_number;
+	this.cust_email_id = cust_email_id;
+	Cust_registration.type_of_customer = type_of_customer;
 }
 
+
+public String getCust_name() {
+	return cust_name;
 }
+
+
+public void setCust_name(String cust_name) {
+	this.cust_name = cust_name;
+}
+
+
+public long getCust_ph_number() {
+	return cust_ph_number;
+}
+
+
+public void setCust_ph_number(long cust_ph_number) {
+	this.cust_ph_number = cust_ph_number;
+}
+
+
+public String getCust_email_id() {
+	return cust_email_id;
+}
+
+
+public void setCust_email_id(String cust_email_id) {
+	this.cust_email_id = cust_email_id;
+}
+
+
+public String getType_of_customer() {
+	return type_of_customer;
+}
+
+
+public void setType_of_customer(String type_of_customer) {
+	Cust_registration.type_of_customer = type_of_customer;
+}
+
+
+public void displayCustomerDetails(){
+	System.out.println("name of the customer"+cust_name);
+	System.out.println("email id of customer"+cust_email_id);
+	System.out.println("phone number of the customer"+cust_ph_number);
+	System.out.println("type of customer:"+type_of_customer);
+
+}
+}
+
